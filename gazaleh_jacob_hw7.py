aec83fb5ad8f30ab2754bbbdd8992d389d354928
Jacob Gazaleh <jessa.gazaleh@c0711a-s30.ufhpc>
2022-04-17 18:38:38 -0400

    correct integration bounds, no graphing

--- gazaleh_jacob_hw7.py
+++ gazaleh_jacob_hw7.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy import units as u
 
+
 open('/home/jessa.gazaleh/AST4930/HW7/sed.txt', 'r')
 sed= np.loadtxt('/home/jessa.gazaleh/AST4930/HW7/sed.txt', delimiter= ",")
 #print(sed)
@@ -11,13 +12,32 @@ sed= np.loadtxt('/home/jessa.gazaleh/AST4930/HW7/sed.txt', delimiter= ",")
 wv= np.asarray(sed[:,0])
 lum= np.asarray(sed[:,1])
 arr=[wv,lum]
-#print(arr)
 
 x=np.where((wv>=10) & (wv<=1000))
-y=np.where((lum>=10) & (lum<=1000))
-print(wv[-1], lum[-1])
-print(wv[0], lum[0])
-print(x)
+s= x[0]
+lowerbound=s[0]
+upperbound=s[-1]
+
+#make the array go from the upper bound to the lower bound
+
+wv_in=[]
+lum_in=[]
+i=lowerbound
+while i<upperbound:
+    wv_in.append(wv[i])
+    lum_in.append(lum[i])
+    i +=1
+
+#print(wv_in)
+#print(lum_in)
+print(upperbound,lowerbound)
+
+print(np.trapz(np.flip(lum_in), np.flip(wv_in)))
+
+
+
+
+#print(x)
 #idk how this works like this, but it gives me the indices of wv that are between 10 and 1000 which is 0 to 833
 
 """
