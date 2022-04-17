36b578288b2a93750620e6e41ffd7cc64094d2a0
Jacob Gazaleh <jessa.gazaleh@c0711a-s30.ufhpc>
2022-04-17 19:21:17 -0400

    this is with units now, no graph

--- gazaleh_jacob_hw7.py
+++ gazaleh_jacob_hw7.py
@@ -1,49 +1,52 @@
-print("hello world")
+#import the useful packages
 import astropy as ast
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy import units as u
 
 
+#import the relevant data
 open('/home/jessa.gazaleh/AST4930/HW7/sed.txt', 'r')
 sed= np.loadtxt('/home/jessa.gazaleh/AST4930/HW7/sed.txt', delimiter= ",")
-#print(sed)
 
+
+#assign an array to each coloumn
 wv= np.asarray(sed[:,0])
 lum= np.asarray(sed[:,1])
-arr=[wv,lum]
 
+#integrate from the correct bounds
+#find where those bounds are and use that in the next step
 x=np.where((wv>=10) & (wv<=1000))
 s= x[0]
 lowerbound=s[0]
 upperbound=s[-1]
 
 #make the array go from the upper bound to the lower bound
-
-wv_in=[]
+wv_in=[] #in means integral
 lum_in=[]
 i=lowerbound
 while i<upperbound:
     wv_in.append(wv[i])
     lum_in.append(lum[i])
     i +=1
+#now we have arrays of the desired integration bounds 
 
-#print(wv_in)
-#print(lum_in)
-print(upperbound,lowerbound)
+#integrate the data with units
+wv_in_unit= wv_in * u.micron
+lum_in_unit= lum_in * u.Lsun/u.micron
+print(np.trapz(np.flip(lum_in_unit), np.flip(wv_in_unit)))
+#this is the integral with units
 
-print(np.trapz(np.flip(lum_in), np.flip(wv_in)))
-
-
-
-
-#print(x)
-#idk how this works like this, but it gives me the indices of wv that are between 10 and 1000 which is 0 to 833
+#this is the integral in different units
+integral=np.trapz(np.flip(lum_in_unit), np.flip(wv_in_unit))
+print(integral.to(u.erg/u.s))
 
 """
-plt.plot(wv, lum)
-plt.set_yscale('log')
-plt.set_xscale('log')
+plt.plot(np.flip(wv), lum)
+plt.yscale("log")
+plt.xscale("log")
+plt.ylabel("Wave length")
+plt.xlabel("Luminosity")
 plt.show()
-plt.savefig('exsamkjhple', dpi=82)
+plt.savefig('example', dpi=82)
 """
